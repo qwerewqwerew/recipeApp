5b209a1922ed92e97b6c49e7d677f52452024910
student-studies-everyday <student.studies.everyday@gmail.com>
2024-04-01 22:13:54 +0900

    chap03-final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-/* import Orange from './Orange'; */
+const fruitDB = [
+	{ id: 1, name: 'banana', image: 'http://qwerew.cafe24.com/images/banana.png' },
+	{ id: 2, name: 'orange', image: 'http://qwerew.cafe24.com/images/orange.png' },
+	{ id: 3, name: 'apple', image: 'http://qwerew.cafe24.com/images/apple.png' },
+	{ id: 4, name: 'melon', image: 'http://qwerew.cafe24.com/images/melon.jpg' },
+];
 
 const Fruit = (props) => {
-  //const fav = props.fav;
-  const {fav}=props;
-  console.log(fav);
-  return <h3>{fav}</h3>;
+	const { name, img } = props;
+	return (
+		<div>
+			<h3>내가 좋아하는 {name}</h3>
+			<img src={img} alt={name} />
+		</div>
+	);
+};
+const renderFruit = (o, i) => {
+	return <Fruit key={i} name={o.name} img={o.image} />;
 };
 
 function App() {
+	console.log(fruitDB.map(renderFruit));
 	return (
 		<div className='App'>
 			<h1>HELLO</h1>
-			<Fruit fav='banana' />
-			<Fruit fav='orange' />
-			<Fruit fav='apple' />
-			<Fruit fav='melon' />
+			{fruitDB.map((o, i) => {
+				return <Fruit key={i} name={o.name} img={o.image} />;
+			})}
+			{/* {fruitDB.map(renderFruit)} */}
 		</div>
 	);
 }
---
+++ src/map.js
@@ -0,0 +1,5 @@
+const frends = ['철수', '영희','복순','민호'];
+const add=frends.map((o)=>{
+return "❤"+o
+})
+console.log(add);
