f5d71f1b7527a69476c4518e16ce0d87fcee8454
qwerewqwerew <qwerew0@gmail.com>
2024-04-01 17:38:48 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+/* import Orange from './Orange'; */
+
+const Fruit = (props) => {
+  //const fav = props.fav;
+  const {fav}=props;
+  console.log(fav);
+  return <h3>{fav}</h3>;
+};
+
 function App() {
-  return (
-    <div className="App">
-      <div>HELLO</div>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>HELLO</h1>
+			<Fruit fav='banana' />
+			<Fruit fav='orange' />
+			<Fruit fav='apple' />
+			<Fruit fav='melon' />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Orange.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Orange = () => {
+	return (
+		<>
+			<h3>Orange</h3>
+		</>
+	);
+};
+export default Orange;
