16215a2141b2307c3c3e37954f58e7938ea36d10
student-studies-everyday <student.studies.everyday@gmail.com>
2024-04-04 22:37:58 +0900

    chap5

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-const fruitDB = [
-	{ id: 1, name: 'banana', image: 'http://qwerew.cafe24.com/images/banana.png' },
-	{ id: 2, name: 'orange', image: 'http://qwerew.cafe24.com/images/orange.png' },
-	{ id: 3, name: 'apple', image: 'http://qwerew.cafe24.com/images/apple.png' },
-	{ id: 4, name: 'melon', image: 'http://qwerew.cafe24.com/images/melon.jpg' },
-];
-
-const Fruit = (props) => {
-	const { name, img } = props;
-	return (
-		<div>
-			<h3>내가 좋아하는 {name}</h3>
-			<img src={img} alt={name} />
-		</div>
-	);
-};
-const renderFruit = (o, i) => {
-	return <Fruit key={i} name={o.name} img={o.image} />;
-};
-
+import { useState, useEffect } from 'react';
 function App() {
-	console.log(fruitDB.map(renderFruit));
-	return (
-		<div className='App'>
-			<h1>HELLO</h1>
-			{fruitDB.map((o, i) => {
-				return <Fruit key={i} name={o.name} img={o.image} />;
-			})}
-			{/* {fruitDB.map(renderFruit)} */}
-		</div>
-	);
+	const [loading, setLoading] = useState({ state: true, data: [] });
+	console.log(loading);
+	useEffect(() => {
+		//레시피데이터로딩
+		setTimeout(() => {
+			setLoading(false);
+		}, 500);
+	}, []);
+	return <div className='App'>{loading ? <h1>로딩중입니다...</h1> : <h1> 요리에몽</h1>}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,21 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import TimerFn from './TimerFn';
-import TimerCl from './TimerCl';
-import Interval from './Interval';
-import LifeCycle from './LifeCycle';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<>
-		<LifeCycle />
-    {/* <Interval /> */}
+		<App />
 	</>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
