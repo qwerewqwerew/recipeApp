18d9485911f067bf783c3bc8a9a58244bbe96ce1
qwerewqwerew <qwerew0@gmail.com>
2024-04-08 16:58:24 +0900

    09

---
+++ src/components/Recipe.js
@@ -0,0 +1,15 @@
+import './Recipe.css';
+const Recipe = ({ name, img_s, img_l, cate }) => {
+	return (
+		<>
+			<div className='list'>
+				<img src={img_s} alt={name} />
+				<div className="text_wrap">
+					<span className='title_txt'>{name}</span>
+					<span className='sub_txt'>{cate}</span>
+				</div>
+			</div>
+		</>
+	);
+};
+export default Recipe;
