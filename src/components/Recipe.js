be07b457eda58885fcc450e2d443b608488c705c
qwerewqwerew <qwerew0@gmail.com>
2024-04-08 17:11:10 +0900

    ch08

---
+++ src/components/Recipe.js
@@ -0,0 +1,14 @@
+import './Recipe.css';
+
+const Recipe = ({ name, img_s, img_l }) => {
+	console.log(name, img_s);
+	return (
+		<>
+			<div className="list">
+				<img src={img_s} alt={name} />
+				<span>{name}</span>
+			</div>
+		</>
+	);
+};
+export default Recipe;
--- src/fetch.js
+++
@@ -1,9 +0,0 @@
-export const fetchDb = async () => {
-	try {
-		let res = await fetch('https://jsonplaceholder.typicode.com/posts');
-		let db = await res.json();
-		return db;
-	} catch (error) {
-		console.error(error);
-	}
-};
