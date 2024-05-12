41168e04fee0b3edf74ffa9077b45ccad8d97656
student-studies-everyday <student.studies.everyday@gmail.com>
2024-05-12 20:50:34 +0900

    chap07 fin

--- src/App1.js
+++ src/App1.js
@@ -2,15 +2,15 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-	const [loading, setLoading] = useState({ state: true, data: [] });
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState([]);
 	const getDB = async () => {
 		try {
 			const { data } = await axios.get('http://openapi.foodsafetykorea.go.kr/api/08cc9b42270a4439b1b5/COOKRCP01/json/1/100');
-			const {
-				COOKRCP01: { row },
-			} = data;
-			setLoading({ state: false, data: row });
-			console.log('data', loading.data);
+      console.log(data);
+			//const {	COOKRCP01: { row } } = data;
+			setLoading(false);
+      //setrcpDB(row);
 		} catch (error) {
 			console.error('데이터를 불러오는데 실패했습니다.', error);
 		}
@@ -24,15 +24,10 @@ function App() {
 			{loading.state ? (
 				<h1>로딩중입니다...</h1>
 			) : (
-				loading.data.map((a) => (
+				data.map((a) => (
 					<div key={a.RCP_SEQ}>
 						<span style={{ color: 'red' }}>{a.RCP_NM}</span>
 						<span>{a.RCP_WAY2}</span>
-						<div>
-							<span>{a.MANUAL01}</span>
-							<span>{a.MANUAL02}</span>
-							<span>{a.MANUAL03}</span>
-						</div>
 					</div>
 				))
 			)}
--- src/App2.js
+++ src/App2.js
@@ -2,15 +2,17 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-	const [loading, setLoading] = useState({ state: true, data: [] });
+	const [loading, setLoading] = useState(true);
+	const [rcpDB, setrcpDB] = useState([]);
 	const getDB = async () => {
 		try {
 			const { data } = await axios.get('http://openapi.foodsafetykorea.go.kr/api/08cc9b42270a4439b1b5/COOKRCP01/json/1/100');
 			const {
 				COOKRCP01: { row },
 			} = data;
-			setLoading({ state: false, data: row });
-			console.log('data', loading.data);
+			console.log(data);
+			setLoading(false);
+			setrcpDB(row); // 데이터를 row로 직접 설정
 		} catch (error) {
 			console.error('데이터를 불러오는데 실패했습니다.', error);
 		}
@@ -21,20 +23,19 @@ function App() {
 
 	return (
 		<div className='App'>
-			{loading.state ? (
+			{loading ? ( // 로딩 상태 체크
 				<h1>로딩중입니다...</h1>
 			) : (
-				loading.data.map((a) => (
-					<div key={a.RCP_SEQ}>
-						<span style={{ color: 'red' }}>{a.RCP_NM}</span>
-						<span>{a.RCP_WAY2}</span>
-						<div>
-							<span>{a.MANUAL01}</span>
-							<span>{a.MANUAL02}</span>
-							<span>{a.MANUAL03}</span>
+				rcpDB.map(
+					(
+						a // rcpDB 배열 직접 매핑
+					) => (
+						<div key={a.RCP_SEQ}>
+							<span style={{ color: 'red' }}>{a.RCP_NM}</span>
+							<span>{a.RCP_WAY2}</span>
 						</div>
-					</div>
-				))
+					)
+				)
 			)}
 		</div>
 	);
